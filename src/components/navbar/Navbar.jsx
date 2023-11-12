04ce4cbd4fcc3c9231c899d5acb3fb1f8ae9ef34
jorgeprj <jlmp2002@gmail.com>
2023-11-12 00:26:24 -0300

    fix: warnings

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import './Navbar.css'
 const Navbar = () => {
     return (
         <div className='navbar'>
-            <div class="logo-container">
-                    <div class="logo">Klutch</div>
+            <div className="logo-container">
+                    <div className="logo">Klutch</div>
             </div>
         </div>
     )

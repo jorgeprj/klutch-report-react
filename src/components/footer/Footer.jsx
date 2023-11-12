e724f6589dd07b00fac0f059e1a55f1a13f70c50
jorgeprj <jlmp2002@gmail.com>
2023-11-12 00:25:57 -0300

    feat: footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+	<footer>
+		<div className="footer-content">
+			<p>&copy; 2023 KLUTCH. All rights reserved.</p>
+		</div>
+	</footer>
+  )
+}
+
+export default Footer

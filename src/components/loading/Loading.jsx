7df74064f82f648bf6dd5cdf485f68f795db6c1e
jorgeprj <jlmp2002@gmail.com>
2023-11-12 00:26:05 -0300

    feat: loading

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Loading.css'
+
+const Loading = () => {
+    return (
+        <div className='section'>
+            <div className='loading'>
+                <div class="lds-dual-ring"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Loading
